fae57b4d5d6ed10a9269c93dd0a8c1a31d670566
Christina Kim <christina.kim@sjsu.edu>
2025-10-10 13:31:34 -0700

    design basic UI of fragment_split.xml

---
+++ app/src/main/java/edu/sjsu/android/expensesplit/SplitFragment.java
@@ -0,0 +1,51 @@
+package edu.sjsu.android.expensesplit;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SplitFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SplitFragment extends Fragment {
+
+    public SplitFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SplitFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SplitFragment newInstance(String param1, String param2) {
+        SplitFragment fragment = new SplitFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_split, container, false);
+    }
+}
